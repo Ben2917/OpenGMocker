de8d24b1c08bae57bd6b62fa37306e5027064b5e
Ben2917 <ben290996@gmail.com>
2023-06-02 17:00:13 +0100

    Beginnings of basic VS extension

---
+++ OpenGMockerExtension/RunOpenGMockerDialogWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenGMockerExtension
+{
+    public partial class RunOpenGMockerDialogWindow : DialogWindow
+    {
+        public RunOpenGMockerDialogWindow()
+        {
+            InitializeComponent();
+            this.MinWidth = this.Width;
+            this.MinHeight = this.Height;
+            this.MaxWidth = this.Width;
+            this.MaxHeight = this.Height;
+        }
+
+        private void SelectInputFileButtonClick(object sender, RoutedEventArgs e)
+        { 
+
+        }
+
+        private void SelectOutputFileButtonClick(object sender, RoutedEventArgs e) 
+        {
+
+        }
+
+        private void RunButtonClick(object sender, RoutedEventArgs e) 
+        { 
+
+        }
+
+        private void CancelButtonClick(object sender, RoutedEventArgs e) 
+        {
+            this.Close();
+        }
+    }
+}
